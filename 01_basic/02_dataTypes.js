7affd020df04db5050980e6b6bd18234ff986aaf
anjali2407 <90312671+anjali2407@users.noreply.github.com>
2024-02-18 18:35:22 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello")  // we are using node js,not browswer
+
+// Datatypes - 
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined => (empty)
+// symbol => for finding uniqueness .figma tols isko jyada use krti h
+
+
+// Object
+
+console.log(typeof "anjali")
+console.log(typeof undefined);
+console.log(typeof null); //null ko as a object treat kiya jta h 
+
