5a1e47f70a389d878b763dbc4055ca472eae883c
anjali2407 <90312671+anjali2407@users.noreply.github.com>
2024-02-19 17:31:33 +0000

    container config done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" =>33
+// 333acv => NaN
+// true => 1; false=>0
+
+
+// let isLoggedIn = "anjali"
+
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true 
+// 0 => false 
+// " " => false
+// "anjali" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
