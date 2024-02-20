094cfd383188c8e16979869e7379bfe97280d5db
anjali2407 <90312671+anjali2407@users.noreply.github.com>
2024-02-20 15:03:27 +0000

    container config done

---
+++ 01_basic/04_comparsion.js
@@ -0,0 +1,28 @@
+// console.log(2>1)
+// console.log(4<1)
+
+
+// console.log("2">1)
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// equality check == and comparisons > < >= <= work differently.
+// Isliye console.log(null>=0)  yeh statement true return krta kyuki ismein
+// null ko 0 treat kiya jata h .
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//undefined humesha false deta h
+
+
+/*************Strict check (===)****/ 
+
+// strict check value k saath saath datatype bhi check krta h
+
+console.log("2"=== 2);
+console.log("4"==="4");
