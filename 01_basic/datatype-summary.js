6f705feb8cbd9912922f69bbacc9fd365f080853
anjali2407 <90312671+anjali2407@users.noreply.github.com>
2024-02-20 15:25:58 +0000

    container config done

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,45 @@
+// ****** PRIMITIVE DATATYPE ***
+ 
+// 1. string    3. boolean    5.undefined
+// 2. number    4. null       6.symbol 
+//             7.Bigint
+
+
+
+// ******* NONPRIMITIVE DATATYPE(refernce)****
+
+// 1. Array 2.Objects 3.Functions
+
+
+
+// Javascript dynamically typed language h because ismien varibale k type define krna utna imporatnat nhi 
+// rhta h .
+
+// ** decalrtion of symbol***
+// symbol mein same value or same datatype pass bhi kr toh 
+// fir bhi vo true consider nhi kreyega .
+
+// const id =Symbol('123')
+// const anotherId =Symbol('123')
+//  console.log(id === anotherId);
+
+
+
+// array
+//  const heros =["shaktiman", "naagraj","doga"]
+  
+
+// object
+//  let myObj =
+//  {
+//     name:"anjali",
+//     age:22,
+//  }
+
+// function
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+
+
+// typeof -- datatype batata h .
