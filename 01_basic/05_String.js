fcf2886711541a36d0d6c7cc5b4fe705c18238eb
anjali2407 <90312671+anjali2407@users.noreply.github.com>
2024-02-21 15:29:44 +0000

    container config done

---
+++ 01_basic/05_String.js
@@ -0,0 +1,11 @@
+let name ="anjali";
+let age = 21;
+let merge = name + age;
+
+console.log(merge); //traditional way to marge the string
+
+console.log(`My name is ${name}. I'm ${age} years old.`) // best way to merge the string 
+
+let ex = "example";
+console.log(`here we performs the various string operation like toUppeCase, toLowerCase, Char At and many more..
+${ex.charAt(5)}`);
---
+++ 01_basic/06_num_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 400
+
+// const balance = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber =23.99177
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//**********   MATHS  **********//
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-88));
+// console.log(Math.round(7.4));
+// console.log(Math.ceil(8.4));
+// console.log(Math.floor(8.4));
+
+console.log(Math.random()); // ismein value deflect hoti either 0 or  1 
+console.log((Math.random()*10)+1); // ismien values ko 0 se 1 mein convert krta h 
+console.log(Math.random()*10); // ismein values ko decimal se ek shift krke convert krte h
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
